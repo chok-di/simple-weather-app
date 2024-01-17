9e5f80699a80f7f763383730cca4ecbe6d1f05a3
chok-di <dwu233@gmail.com>
2024-01-17 04:44:33 -0500

    add hook to make api call to fetch current weather, weather condition code, and timestamp

---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,36 @@
+import { fetchWeatherApi } from 'openmeteo';
+
+const useWeather = async () => {
+  const params = {
+    "latitude": 52.52,
+    "longitude": 13.41,
+    "current": ["temperature_2m", "weather_code"]
+  };
+
+  const url = "https://api.open-meteo.com/v1/forecast";
+  const responses = await fetchWeatherApi(url, params);
+  const range = (start: number, stop: number, step: number) =>
+    Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+  const response = responses[0];
+
+  const utcOffsetSeconds = response.utcOffsetSeconds();
+  const timezone = response.timezone();
+  const timezoneAbbreviation = response.timezoneAbbreviation();
+  const latitude = response.latitude();
+  const longitude = response.longitude();
+
+  const current = response.current()!;
+
+  const weatherData = {
+    
+      time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+      temperature2m: current.variables(0)!.value(),
+      weatherCode: current.variables(1)!.value(),
+    
+  
+  };
+  console.log(weatherData); 
+  return weatherData;
+}
+
+export default useWeather;
