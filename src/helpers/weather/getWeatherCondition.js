daefe76118a57961b0bc26167ebeefb0372be5f3
chok-di <dwu233@gmail.com>
2024-01-21 20:42:48 -0500

    correct the wrong mapping in getWeatherCondition.js helper function

--- src/helpers/weather/getWeatherCondition.js
+++ src/helpers/weather/getWeatherCondition.js
@@ -1,33 +1,33 @@
 const getWeatherCondition= (code) => {
  switch (code) {
   case 0: return 'Clear sky';
-  case 1:
-  case 2:
-  case 3: return 'Mainly clear, partly cloudy, and overcast';
-  case 45:
-  case 48: return 'Fog and depositioning rime fog';
-  case 51:
-  case 53:
-  case 55: return "Drizzle: Light, moderate, and dense intensity";
-  case 56:
-  case 57: return "Freezing Drizzle: Light and dense intensity";
-  case 61:
-  case 63:
-  case 65: return "Rain: Slight, moderate and heavy intensity";
-  case 66:
-  case 67: return "Freezing Rain: Light and heavy intensity";
-  case 71:
-  case 73:
-  case 75: return "Freezing Rain: Light and heavy intensity";
+  case 1: return 'Mainly clear'
+  case 2: return 'Partly cloudy'
+  case 3: return 'Overcast';
+  case 45: return 'Fog';
+  case 48: return 'Depositioning rime fog';
+  case 51: return 'Drizzle: light';
+  case 53: return 'Drizzle: moderate';
+  case 55: return "Drizzle: dense";
+  case 56: return "Freezing Drizzle: light";
+  case 57: return "Freezing Drizzle: dense";
+  case 61: return "Rain: slight";
+  case 63: return "Rain: moderate";
+  case 65: return "Rain: heavy";
+  case 66: return "Freezing Rain: light"
+  case 67: return "Freezing Rain: heavy";
+  case 71: return "Snow fall: slight";
+  case 73: return "Snow fall: moderate";
+  case 75: return "Snow fall: heavy";
   case 77: return "Snow grains";
-  case 80:
-  case 81:
-  case 82: return "Rain showers: Slight, moderate, and violent";
-  case 85:
-  case 86: return "Snow showers slight and heavy";
+  case 80: return "Rain showers: slight";
+  case 81: return "Rain showers: moderate";
+  case 82: return "Rain showers: violent";
+  case 85: return "Snow showers slight";
+  case 86: return "Snow showers heavy";
   case 95: return "Thunderstorm: Slight or moderate";
-  case 96:
-  case 99: return "Thunderstorm with slight and heavy hail"
+  case 96: return "Thunderstorm with slight hail";
+  case 99: return "Thunderstorm with heavy hail";
   default: return "Weather Condition unknown";
  }
 }
